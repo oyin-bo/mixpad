aa669b88a07eeaf03658c99959af1610b2459bcb
Oleg Mihailik <mihailik@gmail.com>
2025-09-14 15:01:10 +0100

    Silly bug!

--- parse/scan-tokens.js
+++ parse/scan-tokens.js
@@ -2,7 +2,7 @@
 
 export const InlineText = 0x1000000;
 export const Whitespace = 0x2000000;
-export const NewLine = 0x1000000;
-export const EntityNamed = 0x3000000;
-export const EntityDecimal = 0x4000000;
-export const EntityHex = 0x5000000;
+export const NewLine = 0x3000000;
+export const EntityNamed = 0x4000000;
+export const EntityDecimal = 0x5000000;
+export const EntityHex = 0x6000000;
